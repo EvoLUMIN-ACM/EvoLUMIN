b26c54f6588639925ae3221b6e4be384ba9b9b74
Locateme-GPS <gowripshenoy@gmail.com>
2024-06-26 20:26:21 +0530

    timeline content update
    timeline content update

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -7,25 +7,25 @@ const Timeline = () => {
   return (
     <Section crosses id="timeline" className="relative lg:py-5 xl:py-[5.8rem] pb-0 lg:pb-0 xl:pb-0">
       <div className="container lg:flex">
-        <div className="max-w-[25rem]">
+         <div className="max-w-[25rem]">
           <h1 className="h3 mb-4 md:mb-8">
             Timeline
           </h1>
-
-          <ul className="max-w-[22rem] mb-10 md:mb-14">
+          
+          {/* <ul className="max-w-[22rem] mb-10 md:mb-14">
             {collabContent.map((item) => (
               <li className="mb-3 py-3" key={item.id}>
                 <div className="flex items-center">
                   <img src={check} width={24} height={24} alt="check" />
                   <h6 className="body-2 ml-5">{item.title}</h6>
                 </div>
-                {/* Remove unwanted text */}
-                {/* {item.text && (
+                { Remove unwanted text }
+                { {item.text && (
                   <p className="body-2 mt-3 text-n-4">{item.text}</p>
-                )} */}
+                )} }
               </li>
             ))}
-          </ul>
+          </ul> */}
 
         </div>
 
