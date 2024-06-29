f1772a9ec8d3aeaf383e89290295c640ea26f8f7
ADITHYA S NAIR <adithyasnair2021@gmail.com>
2024-06-30 02:36:32 +0530

    hiding sponsors
    hiding sponsors

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <Pricing />
         <Tracks />
         {/* <Sdg /> */}
-        <Sponsors/>
+        {/* <Sponsors/> */}
         {/* <Roadmap /> */}
         <Faq/>
         <Footer />
