1561eb4db7ff0e0965d1286a47f5c199322cc6af
Navth <am.en.u4cse22039@am.students.amrita.edu>
2024-09-04 15:31:46 +0530

    new changes to notmain branch .dont accept the pull request

---
+++ src/pages/test.jsx
@@ -0,0 +1,18 @@
+import { Box, Typography } from "@mui/material";
+
+//dont accept the pull request ffs
+
+
+
+export default function Test(){
+  return(
+    <Box>
+      <Typography variant="h1">Test</Typography>
+      <Typography>
+        This is just a test page to see if i commit to some non accessable repo ,sent pull requests and stuffs
+      </Typography>
+
+    </Box>
+
+  )
+}
